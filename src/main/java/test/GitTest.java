03f3cb39469189065a120cdc78a0d71faac3cc43
Wu <Wu@aliyun.com>
2021-05-04 17:38:55 +0800

    first commit

---
+++ src/main/java/test/GitTest.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @Description:
+ * @author:w
+ * @date: 2021-05-04 17:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+    }
+}
