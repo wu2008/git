599a0cabdb1dd1ad5c72beb6e12c1b9ac63ac03e
wu2008 <83599858+wu2008@users.noreply.github.com>
2021-05-04 18:25:48 +0800

    pull commit

--- src/main/java/test/GitTest.java
+++ src/main/java/test/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!!");
         System.out.println("hello git2!!");
         System.out.println("push test!!");
+        System.out.println("pull test!!");
     }
 }
