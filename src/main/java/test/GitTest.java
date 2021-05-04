1526d1b0d303c2a30a9c377fdcd32800eed1375d
Wu <Wu@aliyun.com>
2021-05-04 20:58:51 +0800

    gitee commit

--- src/main/java/test/GitTest.java
+++ src/main/java/test/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2!!");
         System.out.println("push test!!");
         System.out.println("pull test!!");
+        System.out.println("gitee test!!");
     }
 }
